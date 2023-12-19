1d402078e98dbb257ec4e56b8b2754cfc0ac1d88
Adriano <adriano_alves_2008@hotmail.com>
2023-12-19 19:34:57 -0300

    Style: add padding home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -86,7 +86,7 @@ export default function Home() {
   }
 
   return (
-    <div className="w-full flex justify-center items-center min-h-[100vh] bg-violet-700 text-violet-700">
+    <div className="w-full flex justify-center items-center min-h-[100vh] bg-violet-700 text-violet-700 p-2">
       <div className="flex flex-col justify-start items-center p-4 max-w-4xl w-[90%] min-h-[80vh] bg-white rounded-lg">
         <div className="flex flex-col items-center">
           <h2 className="text-[24px] font-bold">Tarefas diárias</h2>
